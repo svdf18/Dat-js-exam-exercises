bbc62308f45a0548b7d2297a032cb896153a9c69
svdf18 <simonvandeurs@hotmail.com>
2023-05-30 10:02:07 +0200

    oe_0

--- environments/environment-00/script.js
+++ environments/environment-00/script.js
@@ -1,1 +1,15 @@
 "use strict";
+
+window.addEventListener("load", initApp);
+
+function initApp(){
+    document.querySelector("#btn-knap").addEventListener("click", clicked)
+    document.querySelector("#result_success").classList.add("hide");
+    document.querySelector("#result_failure").classList.add("hide");
+}
+
+function clicked(){
+    console.log("det virker")
+
+    document.querySelector("#result_success").classList.add("show");
+}
