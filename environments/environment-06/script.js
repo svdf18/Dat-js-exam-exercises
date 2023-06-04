dd359862e5b1303f3be1773cc773d144adc375ff
svdf18 <simonvandeurs@hotmail.com>
2023-06-04 13:01:35 +0200

    done
    only console log. add to basket (global var), remove from basket (global var)

--- environments/environment-06/script.js
+++ environments/environment-06/script.js
@@ -1,1 +1,47 @@
 "use strict";
+
+let products = "";
+let basket = [];
+
+window.addEventListener("load", initApp);
+
+async function initApp(){
+  products = await getData();
+  console.log(products)
+}
+
+async function getData(){
+  const response = await fetch("products.json");
+  const data = await response.json();
+
+  return data;
+}
+
+function addToBasket(productName, quantity){
+  const existingProduct = basket.find(item => item.name === productName);
+
+  if (existingProduct) {
+    existingProduct.quantity += quantity;
+  } else {
+    const product = products.find(item => item.name === productName);
+    if (product) {
+      basket.push({ ...product, quantity });
+    } else {
+      console.log("Product not found");
+    }
+  }
+}
+
+
+function removeFromBasket(productName, quantity){
+  const existingProduct = basket.find(item => item.name === productName);
+
+  if(existingProduct) {
+    existingProduct.quantity -= quantity;
+    if (existingProduct.quantity < 0) {
+      existingProduct.quantity = 0;
+    }
+  } else {
+    console.log("No such item in basket");
+  }
+}
