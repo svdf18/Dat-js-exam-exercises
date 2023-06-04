055a36e5b01ccd2daa45d6b06cf4068cc0355c38
svdf18 <simonvandeurs@hotmail.com>
2023-06-04 10:55:03 +0200

    done
    import, display, display sorted

--- environments/environment-05/script.js
+++ environments/environment-05/script.js
@@ -1,1 +1,30 @@
-"use strict";
+import {courses} from "./courses.js"
+
+window.addEventListener("load", initApp);
+
+function initApp(){
+  console.log(courses); 
+  sortCoursesByEcts();
+  addCourses();
+ 
+}
+
+function addCourses(){
+  for (const course of courses){
+    showCourse(course)
+  }
+}
+
+function showCourse(course){
+  document.querySelector("#courses-list").insertAdjacentHTML(
+    "beforeend", `
+    <p>Course name: ${course.name}</p>
+    <p>Ects points: ${course.ectsPoints}</p>
+    `
+  )
+}
+
+function sortCoursesByEcts(){
+  courses.sort((a, b) => a.ectsPoints - b.ectsPoints);
+}
+
