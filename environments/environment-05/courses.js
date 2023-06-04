dc50fbbaca59e95992efbc47bc67d3633c9f5b6d
svdf18 <simonvandeurs@hotmail.com>
2023-06-04 11:37:43 +0200

    done
    import, display, sort by date (new Date to get a string for compare). To check, ændrer i datoerne i courses.js

--- environments/environment-05/courses.js
+++ environments/environment-05/courses.js
@@ -40,3 +40,5 @@ const courses = [
         teacher: "Tine Jensen"
     }
 ];
+
+export {courses}
--- environments/environment-05/script.js
+++ environments/environment-05/script.js
@@ -1,1 +1,29 @@
-"use strict";
+import {courses} from "./courses.js";
+
+window.addEventListener("load", initApp);
+
+function initApp(){
+  console.log(courses);
+  addCourses();
+}
+
+function addCourses(){
+  for (const course of courses){
+    sortCoursesByDate();
+    showCourse(course);
+  }
+}
+
+function showCourse(course){
+  document.querySelector("#courses-list").insertAdjacentHTML(
+    "beforeend", `
+    <p>Course name: ${course.name}</p>
+    <p>Start date: ${course.startDate}</p>
+    <p>Ects points: ${course.ectsPoints}</p>
+    `
+  )
+}
+
+function sortCoursesByDate() {
+  courses.sort((a, b) => new Date(a.startDate) - new Date(b.startDate));
+}
