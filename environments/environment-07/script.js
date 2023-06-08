03e4c3d3d7558c930ea86c853fa632f72dab400a
svdf18 <simonvandeurs@hotmail.com>
2023-06-08 12:58:35 +0200

    done
    create student, show student, show student < 18, filter alpha

--- environments/environment-07/script.js
+++ environments/environment-07/script.js
@@ -1,1 +1,60 @@
 "use strict";
+
+let students = [];
+
+window.addEventListener("load", initApp);
+
+function initApp(){
+  console.log(students);
+
+  document.querySelector("#create-student-form").addEventListener("submit", createStudentClicked);
+}
+
+function createStudent(name, email, age){
+  const student = {
+    name: name,
+    email: email,
+    age: age,
+  }
+
+  students.push(student);
+
+  document.querySelector("#students-table-body").innerHTML = "";
+
+  addSortedStudents();
+}
+
+function createStudentClicked(event){
+  event.preventDefault();
+  console.log("clicked");
+
+  let name = document.querySelector("#name").value;
+  let email = document.querySelector("#email").value;
+  let age = document.querySelector("#age").value;
+
+  createStudent(name, email, age);
+
+  document.querySelector("#create-student-form").reset();
+}
+
+function addSortedStudents(){
+  const filteredStudents = students.filter(student => student.age > 18);
+  filteredStudents.sort((a, b) => a.name.localeCompare(b.name));
+
+  for (const student of filteredStudents){
+    showStudent(student);
+  }
+}
+
+
+function showStudent(student){
+  document.querySelector("#students-table-body").insertAdjacentHTML(
+    "beforeend", `
+    <tr>
+      <td>${student.name}</td>
+      <td>${student.email}</td>
+      <td>${student.age}</td>
+    </tr>
+    `
+  );
+}
