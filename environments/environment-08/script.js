cc3c3a83ce1ef0a948bdca42a2246c284ac5ccb4
svdf18 <simonvandeurs@hotmail.com>
2023-06-09 11:55:15 +0200

    done
    list, add track, add upvote button, manipulate index

--- environments/environment-08/script.js
+++ environments/environment-08/script.js
@@ -1,1 +1,54 @@
 "use strict";
+
+let playList = [
+{
+  artist: "Freddie Gibbs",
+  title: "Pinata",
+  duration: "3:21"
+},
+{
+  artist: "Lil Uzi Vert",
+  title: "Rock",
+  duration: "3:33"
+},
+{
+  artist: "Blawan",
+  title: "Garage",
+  duration: "4:32"
+}
+];
+
+window.addEventListener("load", initApp);
+
+function initApp() {
+  console.log(playList);
+  addPlaylistItem();
+}
+
+function addPlaylistItem() {
+  for (const track of playList) {
+    document.querySelector("#songlist").insertAdjacentHTML(
+      "beforeend", `
+      <li>ARTIST: ${track.artist}, TITLE: ${track.title}, ${track.duration} <button>Upvote</button></li>
+      `
+    );
+
+    document.querySelector("#songlist li:last-child button").addEventListener("click", () => moveTrackUp(track));
+  }
+}
+
+function moveTrackUp(track) {
+  console.log(track)
+
+  const index = playList.findIndex(item => item === track);
+  console.log(index);
+
+  if (index > 0) {
+    const temp = playList[index - 1];
+    playList[index - 1] = track;
+    playList[index] = temp;
+
+    document.querySelector("#songlist").innerHTML = "";
+    addPlaylistItem();
+  }
+}
